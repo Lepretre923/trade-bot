84be558f00a027f8e24d7549cb8c975c0b1f7c4a
Lepretre923 <nourqg2.0@gmail.com>
2026-03-14 11:56:20 +0100

    Update market.py

--- market.py
+++ market.py
@@ -1,36 +1,78 @@
 import requests
 
+
+# PRIX D'UNE CRYPTO
 def get_price(symbol="BTC"):
 
-    url=f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}USDT"
+    try:
 
-    data=requests.get(url).json()
+        url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}USDT"
 
-    return f"💰 Prix {symbol}\n\n{data['price']} $"
+        data = requests.get(url).json()
+
+        price = float(data["price"])
+
+        return f"""
+💰 PRIX {symbol}
+
+Prix actuel
+
+{price:.2f} $
+
+Source
+Binance Spot
+"""
+
+    except:
+
+        return "Erreur récupération prix marché"
 
 
+# TOP CRYPTOS
 def top_crypto():
 
-    url="https://api.coingecko.com/api/v3/coins/markets"
+    try:
 
-    params={
-        "vs_currency":"usd",
-        "order":"market_cap_desc",
-        "per_page":10,
-        "page":1
-    }
+        url = "https://api.coingecko.com/api/v3/coins/markets"
 
-    data=requests.get(url,params=params).json()
+        params = {
+            "vs_currency": "usd",
+            "order": "market_cap_desc",
+            "per_page": 10,
+            "page": 1
+        }
 
-    msg="🚀 Top cryptos\n\n"
+        data = requests.get(url, params=params).json()
 
-    for coin in data[:5]:
+        msg = "🚀 TOP CRYPTOS MARCHÉ\n\n"
 
-        msg+=f"{coin['name']} : {coin['price_change_percentage_24h']:.2f}%\n"
+        for coin in data[:5]:
 
-    return msg
+            name = coin["name"]
+            change = coin["price_change_percentage_24h"]
+
+            direction = "🟢" if change > 0 else "🔴"
+
+            msg += f"{direction} {name}  {change:.2f}%\n"
+
+        msg += "\nAnalyse\n"
+        msg += "Comparer la performance\n"
+        msg += "des leaders du marché."
+
+        return msg
+
+    except:
+
+        return "Erreur récupération données marché"
 
 
+# GRAPH BTC
 def btc_chart():
 
-    return "📊 Graphique BTC\nhttps://www.tradingview.com/chart/?symbol=BINANCE:BTCUSDT"
+    return """
+📊 GRAPHIQUE BTC
+
+Voir analyse technique
+
+https://www.tradingview.com/chart/?symbol=BINANCE:BTCUSDT
+"""
